64cbf88e030949f733b107263968c4519f03ab78
George Aidonidis <george.aidonidis@gmail.com>
2018-06-12 18:45:03 +0300

    Setup things

---
+++ readme.md
@@ -0,0 +1,35 @@
+# mocking-currency-conversion-graphql
+
+<!-- TOC depthFrom:2 depthTo:6 withLinks:1 updateOnSave:0 orderedList:0 -->
+
+- [What's this?](#whats-this)
+- [How can I use it?](#how-can-i-use-it)
+- [Where is this deployed? :rocket:](#where-is-this-deployed-rocket)
+
+<!-- /TOC -->
+
+### What's this?
+A graphql server that provides currency rates from [OpenExchange](http://openexchangerates.org).
+
+### How can I use it?
+Here is an example:
+
+* base: String --> The base to calculate the rest of the currencies
+* currencies: [{abbreviation: String, rate: String}] --> The currencies to return (if no input is given will return all the currencies).
+
+```GraphQL
+query {
+  getConversions(base: "USD", currencies: ["EUR", "GBP", "AUR"]) {
+    base,
+    timestamp
+    rates {
+      abbreviation
+      rate
+    }
+  }
+}
+```
+
+### Where is this deployed? :rocket:
+
+It's hosted on https://graphql-currencies.now.sh
---
+++ src/index.js
@@ -0,0 +1,79 @@
+const { makeExecutableSchema } = require('graphql-tools');
+const { GraphQLServer } = require('graphql-yoga')
+const fetch = require('isomorphic-fetch');
+const GraphQLJSON = require('graphql-type-json');
+const monk = require('monk')
+const {map, keys, isNil, isEmpty} = require('ramda')
+const {typeDefs} = require('./type-defs')
+
+const url = process.env.MONGODB_URI;
+
+const db = monk(url);
+
+const fetchRates = async () => {
+	const res = await fetch(`https://openexchangerates.org/api/latest.json?app_id=${process.env.API_KEY}`)
+	return res.json();
+}
+
+const saveRates = async () => {
+	const rates = await fetchRates();
+	const bases = keys(rates.rates);
+
+	const collection = db.get('rates')
+
+	bases.forEach(base => {
+
+		const rate = keys(rates.rates).map(key => ({
+			abbreviation: key,
+			rate: rates.rates[key]/rates.rates[base]
+		}));
+
+		const conversionRate = {
+			base,
+			rates: rate,
+			timestamp: rates.timestamp
+		}
+
+		collection.findOneAndUpdate(
+			{ base: conversionRate.base },
+			{ $currentDate: { lastModified: true }, $set: conversionRate },
+			{ upsert: true, returnNewDocument: true },
+		);
+	});
+}
+
+const buildRates = (rates, currencies) => {
+	if (!isNil(currencies) && !isEmpty(currencies)) {
+		return currencies.map(currency => (rates.find(rate => rate.abbreviation === currency)));
+	}
+	return rates;
+}
+
+const getFromRatesFromDb = base => {
+	const collection = db.get('rates');
+	return collection.findOne({ base });
+}
+
+const resolvers = {
+	Query: {
+		conversionRate: async (_, { base, currencies }) => {
+			const rates = await getFromRatesFromDb(base);
+
+			const euro = map(r => r/rates.rates['EUR'])(rates.rates);
+
+			return {
+				timestamp: rates.timestamp,
+				base: rates.base,
+				rates: buildRates(rates.rates, currencies)
+			}
+		}
+	},
+}
+const server = new GraphQLServer({ typeDefs, resolvers });
+saveRates();
+server.start(
+	{port: process.env.PORT || 3000},
+	() => {
+		console.log(`Server is running on localhost:${process.env.PORT}`);
+	}
+)
